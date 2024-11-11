4ebb14f3a6945f01f9a9ee85cccd88fd85d20e48
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-12 01:31:36 +0600

    added conditional javascritp code

---
+++ js loop/forloop.js
@@ -0,0 +1,5 @@
+const fruits = ['orange', 'apple', 'mango', 'lemon', 'watermelon'];
+for (const fruit of fruits) {
+    console.log(fruit);
+    console.log('i want to eat');
+}
---
+++ js loop/whileloop.js
@@ -0,0 +1,12 @@
+const number = 30;
+if (number < 20) {
+    console.log('10 is less than of 20');
+}
+console.log('something is happened');
+// ----------while loop----------------
+let num = 0;
+while (num < 10) {
+    console.log('looping', num);
+    // num = num + 1;
+    num++;
+}
