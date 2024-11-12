f1bc3a8630446715293c008a863d2f60ba9b2d5c
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-12 23:59:42 +0600

    added slice string

---
+++ js loop/js string vs array/stringslice.js
@@ -0,0 +1,7 @@
+const address = 'Dhaka';
+const part = address.slice(1, 3);
+console.log(part);
+
+const sentence = 'I am a good and hardworking person.';
+console.log(sentence.split(' '));
+console.log(sentence.split('a'));
