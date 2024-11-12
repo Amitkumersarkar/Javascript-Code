267e51bd2de91e0bd6da619b0ac151cdcb7fdd92
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-12 23:45:21 +0600

    added string file

---
+++ js loop/js string vs array/case.js
@@ -0,0 +1,24 @@
+// comparison two string
+const university = 'Dhaka University';
+console.log(university);
+console.log(university.toLowerCase());
+console.log(university.toUpperCase());
+
+// uppercase to lowercase conversion
+// lowercase to uppercase  conversion
+const subject = 'Statistic';
+const book = 'staTistiC';
+if (subject.toLowerCase() === book.toLowerCase()) {
+    console.log('i will get height gpa');
+}
+else {
+    console.log('i will try harder');
+}
+const drink = 'water';
+const liquid = 'water ';
+if (drink.trim() === liquid.trim()) {
+    console.log('water is the life savor');
+}
+else {
+    console.log('sea water are so salty');
+}
---
+++ js loop/js string vs array/string1.js
@@ -0,0 +1,17 @@
+// const country = 'india';
+// const division = 'mumbai';
+// const district = `mumbai`;
+// back tick sign (``)
+// const thana = new String('mumbai');
+
+// console.log(district);
+
+// const number = [10, 20, 30, 40];
+// console.log(number.length);
+// console.log(number[2]);
+const capital = 'Dhaka';
+console.log(capital.length)
+console.log(capital[2]);
+capital[1] = 'o';
+// string is immutable --> not changeable
+// array is mutable and you can change the array element
