1a3e49db74480e595cc1d7eb7490b32b57271373
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-13 01:44:42 +0600

    added oop file

---
+++ js loop/js string vs array/objects.js
@@ -0,0 +1,7 @@
+const age = 23;
+const collage = 'wilfc';
+const isPassed = true;
+// undefine value
+let isDeveloper;
+const subjests = ['bangla', 'English', 'math'];
+console.log(isDeveloper);
---
+++ js loop/oop in javascript/object1.js
@@ -0,0 +1,21 @@
+// oject declaration system
+const bottle = {
+    //1st property 2n value , 3rd color
+    brand: 'coca-cola',
+    price: 50,
+    color: black,
+    isClean: true,
+}
+// after declaring object/property you have to give colon then value then comma.
+// object type is a non primitive
+// basic,
+const subject = {
+    name: 'physics',
+    teacher: 'taskia mam',
+    examDate: '10 nov 2024',
+    chapter: ['velocity', 'motions', 'force'],
+    exams: {
+        name: 'final exam',
+        marks: 70,
+    }
+}
