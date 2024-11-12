9d19376dfd4ae22d40d955502470cc797c1a12e0
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-13 00:11:16 +0600

    added string slicing

--- js loop/js string vs array/stringslice.js
+++ js loop/js string vs array/stringslice.js
@@ -1,7 +1,13 @@
-const address = 'Dhaka';
-const part = address.slice(1, 3);
-console.log(part);
+// const address = 'Dhaka';
+// const part = address.slice(1, 3);
+// console.log(part);
 
-const sentence = 'I am a good and hardworking person.';
-console.log(sentence.split(' '));
-console.log(sentence.split('a'));
+// const sentence = 'I am a good and hardworking person.';
+// console.log(sentence.split(' '));
+// console.log(sentence.split('a'));
+
+const friendsStr = 'anik , amit, amrit, akash';
+const friend = friendsStr.split(',');
+console.log(friend);
+const realFriends = ['anik ', ' amit', ' amrit', ' akash'];
+console.log(realFriends.join('-'));
