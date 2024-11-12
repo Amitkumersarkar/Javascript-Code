7ec8358bd242fda8bf5a6577cf20acfa744eca9b
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-13 00:25:01 +0600

    added new code

---
+++ js loop/js string vs array/concat.js
@@ -0,0 +1,13 @@
+const first = 'amit';
+const last = 'amrit';
+// const fullName = first + last;
+// white space in the middle of the name
+const fullName = first + ' ' + last;
+// console.log(fullName);
+const fullName2 = first.concat(' ').concat(last);
+console.log(fullName2);
+
+// includes
+
+// console.log(last.includes('d'));
+console.log(last.includes('a'));
