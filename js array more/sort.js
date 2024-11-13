77a8b121c116a9d7e937e2c4d7eaefad9bf82bd4
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-14 01:02:59 +0600

    added sort js code

---
+++ js array more/sort.js
@@ -0,0 +1,11 @@
+const persons = ['Amit', 'anik', 'Amrit', 'akash'];
+const shortedPersons = persons.sort();
+console.log(shortedPersons);
+// sort--->
+const numbers = [4, 7, 12, 20]
+// ascending--->smaller to larger : [4,7,12,20]
+
+// descending ---> larger to smaller: [20,12,7,4]
+
+const numbers_asc = numbers.sort()
+console.log(numbers_asc);
