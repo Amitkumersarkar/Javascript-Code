ca2462feb71d5fc62179dcd180d1dac69c010024
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-14 00:27:18 +0600

    added js looping

---
+++ js array more/looping.js
@@ -0,0 +1,30 @@
+
+const friends = ['Elon', 'mark', 'bill', 'waren'];
+for (const friend of friends) {
+    // console.log(friend);
+}
+for (let i = 0; i < 20; i++) {
+    //     console.log(i);
+    // console.log(friends[i]);
+
+}
+
+const numbers = [10, 22, 30, 40, 5, 6, 50, 80];
+for (let i = 0; i < numbers.length; i++) {
+
+    // console.log(numbers[i]);
+
+}
+
+let i = 0;
+while (i < friends.length) {
+    // console.log(friends[i];
+
+    i++;
+}
+
+let n = 0;
+while (n < numbers.length) {
+    console.log(numbers[n])
+    n++;
+}
