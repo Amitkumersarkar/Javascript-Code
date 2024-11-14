9c548303e732f446b16fd2e1908620c5c47c9754
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-15 00:07:08 +0600

    added parameterize function

---
+++ Js Function/parameter.js
@@ -0,0 +1,18 @@
+function square(number) {
+    // console.log(number);
+    console.log('value of the parameter:', number);
+    const sqr = number * number;
+    console.log('square of the given number is : ', sqr);
+}
+square(8);
+
+// another function with two parameter
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+    console.log('value of the two parameter is : ');
+    // console.log('value of the second parameter is : ');
+    console.log(sum);
+}
+add(4, 5);
+add(8, 10);
---
+++ Js Function/parameter2.js
@@ -0,0 +1,6 @@
+function addAll(a, b, c, d, e) {
+    const sum = a + b + c + d + e;
+    console.log(a, b, c, d, e);
+    console.log(sum);
+}
+addAll(3, 5, 4, 2, 6);
