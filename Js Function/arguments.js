49cb461ffe71639dd88659fbd97b6bfb71b231b2
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-15 00:37:28 +0600

    added js  argument

---
+++ Js Function/arguments.js
@@ -0,0 +1,8 @@
+function difference(num1, num2) {
+    const diff = num1 - num2;
+    console.log(num1, num2, 'difference is', diff);
+
+}
+const fatherAge = 50;
+const myAge = 23;
+difference(fatherAge, myAge);
