2e7a2ae62e7b625f92e5d220822e66c55477dd59
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-15 00:59:06 +0600

    added js returns value

---
+++ Js Function/setvalue.js
@@ -0,0 +1,12 @@
+function tenTimes(number) {
+    const result = number * 20;
+    return result;
+}
+function cutHalf(number) {
+    const half = number / 2;
+    return half;
+}
+tenTimes(5);
+const output = tenTimes(5);
+console.log('output', output);
+const bigNumber = tenTimes(99);
