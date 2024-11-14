4b6b6518fb6634195c54d0fab9c577025d01e3de
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-14 23:28:15 +0600

    added function file

---
+++ Js Function/function.js
@@ -0,0 +1,10 @@
+const number = 45;
+// function declaration
+function fanOff() {
+    console.log('click the switch to stop the fan');
+    console.log('click the switch to start the fan');
+
+}
+// function call
+fanOff();
+
