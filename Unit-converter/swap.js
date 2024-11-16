1ab67e5af0055ff1f8188c0f8d127f290127264c
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-16 16:25:53 +0600

    added swapping in js

---
+++ Unit-converter/swap.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+
+
+console.log(a, b);
+
+// a = b;
+
+// b = a;
+
+const temp = a;
+a = b;
+b = temp;
+console.log(a, b);
---
+++ Unit-converter/swap2.js
@@ -0,0 +1,6 @@
+// without temp 
+let x = 50;
+let y = 30;
+console.log(x, y);
+[x, y] = [y, x];
+console.log(x, y);
