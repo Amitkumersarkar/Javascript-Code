df61e19eac98d6d9b79a07f31d074eb5fa32f4ac
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-19 01:20:32 +0600

    added js practice task2

---
+++ Js-exercise-01/task2.js
@@ -0,0 +1,12 @@
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+var Mathematics = 75.25;
+var Biology = 75;
+var Chemistry = 80;
+var Physics = 80;
+var English = 90;
+
+var totalMarks = Mathematics + Biology + Chemistry + Physics + English;
+var averageMarks = totalMarks / 5;
+console.log(totalMarks);
+console.log(averageMarks);
